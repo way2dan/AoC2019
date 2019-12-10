ad169fd9cde893bbb17ae5789fc877cb76f1bdc8
Eugene Kruglov <public@kruglov.org>
2019-12-10 18:47:29 +0300

    Add files via upload

--- day10.py
+++ day10.py
@@ -57,8 +57,8 @@ print(best_asteroid)
 i = 0
 while i < 200:
     for p in directions:
-        z = shoot_lazer(belt, best_asteroid[0], best_asteroid[1], p[0], p[1])
-        i += 1 if z else 0
+        vaporized = shoot_lazer(belt, best_asteroid[0], best_asteroid[1], p[0], p[1])
+        i += 1 if vaporized else 0
         if i == 200:
             break
-print(z)
+print(vaporized)
